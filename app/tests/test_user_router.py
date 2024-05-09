bfb700968bde8e2b6d50867601ae7337c1d12d74
Samiiz <skboss159@gmail.com>
2024-05-10 02:03:54 +0900

    feat: 이메일 전송 테스트

--- app/tests/test_user_router.py
+++ app/tests/test_user_router.py
@@ -2,7 +2,11 @@ from unittest.mock import MagicMock, patch
 
 from tortoise.contrib.test import TestCase
 
-from app.services.user_service import generate_verification_code
+from app.dtos.user_response import SendVerificationCodeResponse
+from app.services.user_service import (
+    generate_verification_code,
+    send_verification_email,
+)
 
 
 class TestEmailVerification(TestCase):
@@ -17,3 +21,23 @@ class TestEmailVerification(TestCase):
         self.assertEqual(len(str(code2)), 6)
         self.assertEqual(len(str(code3)), 6)
         self.assertEqual(len(str(code4)), 6)
+
+    def test_send_email(self) -> None:
+
+        gmail_username: str = "its.verified.test@gmail.com"
+        gmail_password: str = "goxvdsfjrovuyqzv"
+
+        receiver_name = "TestUser"
+        receiver_email = "example@example.com"
+
+        request_data = SendVerificationCodeResponse(name=receiver_name, email=receiver_email)
+
+        smtp_mock = MagicMock()
+        with patch("app.services.user_service.smtplib.SMTP_SSL") as mock_smtp:
+            mock_smtp.return_value = smtp_mock
+
+            send_verification_email(request_data)
+
+            mock_smtp.assert_called_once_with("smtp.gmail.com", 465)
+            smtp_mock.login.assert_called_once_with(gmail_username, gmail_password)
+            smtp_mock.sendmail.assert_called_once()
