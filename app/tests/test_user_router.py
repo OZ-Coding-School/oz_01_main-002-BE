79e8a27d169a584d71c6e20a7946031aed806227
Samiiz <skboss159@gmail.com>
2024-05-10 02:13:56 +0900

    fix: 인증 번호 생성 함수 변경에 따른 테스트 변경

--- app/tests/test_user_router.py
+++ app/tests/test_user_router.py
@@ -14,13 +14,8 @@ class TestEmailVerification(TestCase):
     def test_generate_verification_code(self) -> None:
         code1: int = generate_verification_code()
         code2: int = generate_verification_code()
-        code3: int = generate_verification_code()
-        code4: int = generate_verification_code()
 
-        self.assertEqual(len(str(code1)), 6)
-        self.assertEqual(len(str(code2)), 6)
-        self.assertEqual(len(str(code3)), 6)
-        self.assertEqual(len(str(code4)), 6)
+        self.assertNotEqual(code1, code2)
 
     def test_send_email(self) -> None:
 
