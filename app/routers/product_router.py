c78f9b981c3b7c5451aaf446c32cb468fa6fd6fa
hyeongbin0516 <gudqls0516@gmail.com>
2024-05-09 17:03:51 +0900

    fest:product detail 기능 추가 및 코드 수정

--- app/routers/product_router.py
+++ app/routers/product_router.py
@@ -3,7 +3,10 @@ from fastapi import APIRouter
 from app.dtos.product_response import ProductResponse
 from app.services.product_service import (
     service_create_product,
+    service_delete_product,
     service_get_all_products,
+    service_get_by_product_id,
+    service_update_product,
 )
 
 router = APIRouter(prefix="/api/v1/products", tags=["product"], redirect_slashes=False)
@@ -34,3 +37,36 @@ async def router_create_product(request_data: ProductResponse) -> ProductRespons
         grade=grade,
         category=category,
     )
+
+
+@router.get("/{product_id}", response_model=ProductResponse)
+async def router_get_product_id(product_id: int) -> list[ProductResponse]:
+    return await service_get_by_product_id(product_id)
+
+
+@router.put("/{product_id}", response_model=ProductResponse)
+async def router_update_product(product_id: int, request_data: ProductResponse) -> ProductResponse:
+    product_id = request_data.id
+    name = request_data.name
+    content = request_data.content
+    bid_price = request_data.bid_price
+    duration = request_data.duration
+    status = request_data.status
+    grade = request_data.grade
+    category = request_data.category
+    return await service_update_product(
+        product_id=product_id,
+        name=name,
+        content=content,
+        bid_price=bid_price,
+        duration=duration,
+        status=status,
+        grade=grade,
+        category=category,
+    )
+
+
+@router.delete("/{product_id}")
+async def router_delete_product(product_id: int) -> dict[str, str]:
+    await service_delete_product(product_id)
+    return {"message": "Product deleted successfully"}
--- app/services/product_service.py
+++ app/services/product_service.py
@@ -6,22 +6,20 @@ from app.models.products import Product
 
 async def service_get_all_products() -> list[ProductResponse]:
     products = await Product.get_all_by_products()
-    product_list: list[ProductResponse] = []
-    for product in products:
-        product_list.append(
-            ProductResponse(
-                id=product.id,
-                name=product.name,
-                content=product.content,
-                bid_price=product.bid_price,
-                duration=product.duration,
-                status=product.status,
-                modify=product.modify,
-                grade=product.grade,
-                category=product.category,
-            )
+    return [
+        ProductResponse(
+            id=product.id,
+            name=product.name,
+            content=product.content,
+            bid_price=product.bid_price,
+            duration=product.duration,
+            status=product.status,
+            modify=product.modify,
+            grade=product.grade,
+            category=product.category,
         )
-    return product_list
+        for product in products
+    ]
 
 
 async def service_create_product(
@@ -48,3 +46,65 @@ async def service_create_product(
         grade=product.grade,
         category=product.category,
     )
+
+
+async def service_get_by_product_id(product_id: int) -> list[ProductResponse]:
+    product = await Product.get_by_product_id(product_id)
+    if product:
+        return [
+            ProductResponse(
+                id=product.id,
+                name=product.name,
+                content=product.content,
+                bid_price=product.bid_price,
+                duration=product.duration,
+                status=product.status,
+                modify=product.modify,
+                grade=product.grade,
+                category=product.category,
+            )
+        ]
+    raise HTTPException(status_code=404, detail="Product not found")
+
+
+async def service_update_product(
+    product_id: int, name: str, content: str, bid_price: int, duration: int, status: str, grade: str, category: str
+) -> ProductResponse:
+    try:
+        product = await Product.get_by_product_id(product_id)
+    except Exception as e:
+        raise HTTPException(status_code=404, detail="not found")
+
+    await Product.update_by_product_id(
+        id=product.id,
+        name=name,
+        content=content,
+        bid_price=bid_price,
+        duration=duration,
+        status=status,
+        grade=grade,
+        category=category,
+    )
+
+    new_product = await Product.get_by_product_id(product_id)
+
+    return ProductResponse(
+        id=new_product.id,
+        name=new_product.name,
+        content=new_product.content,
+        bid_price=new_product.bid_price,
+        duration=new_product.duration,
+        status=new_product.status,
+        modify=product.modify,
+        grade=new_product.grade,
+        category=new_product.category,
+    )
+
+
+async def service_delete_product(product_id: int) -> None:
+    try:
+        product = await Product.get_by_product_id(product_id)
+    except Exception as e:
+        raise HTTPException(status_code=404, detail="not found")
+
+    await product.delete()
