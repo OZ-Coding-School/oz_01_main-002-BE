6be64cd50900de9c7128831ebbdacd612b716da9
Samiiz <skboss159@gmail.com>
2024-05-12 09:31:00 +0900

    fix: mypy오류로 인한 타입 지정 제외
    redis: "Redis[str]" -> redis

--- app/utils/redis_.py
+++ app/utils/redis_.py
@@ -2,6 +2,6 @@ from redis.asyncio import ConnectionError, Redis
 
 from app.configs import settings
 
-redis: "Redis[str]" = Redis.from_url(
+redis = Redis.from_url(
     settings.REDIS_URL, decode_responses=True, socket_timeout=5, retry_on_timeout=True, retry_on_error=[ConnectionError]
 )
