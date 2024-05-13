04c715be1c1159419ad449e646591717ad64d4b0
Samiiz <skboss159@gmail.com>
2024-05-13 22:38:09 +0900

    chore: base_setting 수정

--- app/configs/base_settings.py
+++ app/configs/base_settings.py
@@ -14,5 +14,8 @@ class Settings(BaseSettings):
     DB_HOST: str = "127.0.0.1"
     DB_PORT: int = 3306
     DB_USER: str = "root"
-    DB_PASSWORD: str = "gudqls0516"
-    DB_DB: str = "fastapi_db"
+    DB_PASSWORD: str = ""
+    DB_DB: str = ""
+    GMAIL_USERNAME: str
+    GMAIL_PASSWORD: str
+    REDIS_URL: str
