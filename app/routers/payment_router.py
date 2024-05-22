caa1158fec047175587b4dc22090b8dff9e6b2c9
Fillsogood <jdd05273@naver.com>
2024-05-22 14:17:50 +0900

    fix: payment jwt 검증 기능 추가 및 테스트 코드 수정

--- app/routers/payment_router.py
+++ app/routers/payment_router.py
@@ -1,16 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from app.dtos.payment_response import PaymentCreateResponse, PaymentGetResponse
 from app.services.payment_service import service_create_payment, service_get_by_payment
+from app.services.user_service import get_current_user
 
 router = APIRouter(prefix="/api/v1/payments", tags=["payment"], redirect_slashes=False)
 
 
 @router.post("/")
-async def router_create_payment(request_data: PaymentCreateResponse) -> None:
-    return await service_create_payment(request_data)
+async def router_create_payment(
+    request_data: PaymentCreateResponse, current_user: int = Depends(get_current_user)
+) -> None:
+    return await service_create_payment(request_data, current_user)
 
 
 @router.get("/{payment_id}/", response_model=PaymentGetResponse)
-async def router_get_by_payment_id(payment_id: int) -> PaymentGetResponse:
+async def router_get_by_payment_id(payment_id: int, _: int = Depends(get_current_user)) -> PaymentGetResponse:
     return await service_get_by_payment(payment_id)
