0dd2bc8aa95ddf873d5fe38503205b0eaf438811
hyeongbin0516 <gudqls0516@gmail.com>
2024-05-24 16:01:27 +0900

    fix: auction 수정

--- app/dtos/auction_response.py
+++ app/dtos/auction_response.py
@@ -8,6 +8,10 @@ from pydantic import BaseModel
 class AuctionResponse(BaseModel):
     id: Optional[int] = None
     product_id: Optional[int] = None
+    product_name: str
+    product_bid_price: int
+    product_grade: str
+    is_active: Optional[str] = None
     status: Optional[bool] = None
     start_time: Optional[datetime] = None
     end_time: Optional[datetime] = None
@@ -19,5 +23,5 @@ class AuctionCreate(BaseModel):
     charge: int
 
 
-class AuctionUpdate(AuctionResponse):
-    status: Optional[bool] = None
+class AuctionUpdate(BaseModel):
+    status: bool
